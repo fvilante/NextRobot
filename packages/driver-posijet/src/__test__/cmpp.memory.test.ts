02d405396d5150fc55526870d83ddeac074247d1
Flavio <fvilante1@gmail.com>
2019-07-26 18:14:19 -0300

    removed some unused debug console.logs

--- packages/driver-posijet/src/__test__/cmpp.memory.test.ts
+++ packages/driver-posijet/src/__test__/cmpp.memory.test.ts
@@ -9,12 +9,12 @@ describe('Template', () => {
     it('test', () => {        
         const expected = CMPP_Memory
         const result = CMPP_Memory
-        console.log(CMPP_Memory)
+        //console.log(CMPP_Memory)
         const newMem = upsertIntoContigousMemory(CMPP_Memory, 10, {
             content: 20,
             lastUpdate: 1000
         })
-        console.log(newMem)
+        //console.log(newMem)
         expect(
             result
         ).toEqual(expected)
