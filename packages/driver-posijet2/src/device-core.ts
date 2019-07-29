6bc387069f8e26c3e4daedc826cf0bd3aea5415c
Flavio <fvilante1@gmail.com>
2019-07-29 15:05:34 -0300

    namming convention

--- packages/driver-posijet2/src/device-core.ts
+++ packages/driver-posijet2/src/device-core.ts
@@ -1,16 +1,13 @@
 
-import { PhysicalLinearAxis } from './physical-arm'
+import { LinearAxisClassic } from './physical-arm'
 import { Channel } from './transport-types'
 import { Memmap, AnyDriver } from './driver-core'
 
 
 
-interface LinearDevice<T extends AnyDriver> {
-    readonly mechanics: PhysicalLinearAxis,
+export interface Device<T extends AnyDriver> {
+    readonly mechanics: LinearAxisClassic,
     readonly serialPortName: string,
     readonly channel: Channel,
     readonly memmap: Memmap<T>
 }
-
-
-export type Device<T extends AnyDriver> = LinearDevice<T>
--- packages/driver-posijet2/src/physical-arm.ts
+++ packages/driver-posijet2/src/physical-arm.ts
@@ -1,9 +1,7 @@
 
 
-export interface PhysicalLinearAxis  {
+export interface LinearAxisClassic  {
     readonly pulsesPerMotorRevolution: 200 | 400, // pulsos por volta do motor
     readonly teethOnTheMotorPulley: 12 | 16 | 20, // numero de dentes da polia do motor
     readonly beltStepInMilimeters: 5.08,          // passo da correia dentada em milimetros
 }
-
-export const PhysicalLinearAxis = (_: PhysicalLinearAxis):PhysicalLinearAxis => _
