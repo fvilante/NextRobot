36edc0a688413775e88c655e5301573118c8bf5c
Flavio <fvilante1@gmail.com>
2019-07-29 17:46:13 -0300

    add homeophorfic map type alias

--- packages/driver-posijet2/src/utils.ts
+++ packages/driver-posijet2/src/utils.ts
@@ -1,4 +1,7 @@
 
+//an alias to number conversion
+export type HomophorficMap<T> = (_: T) => T
+
 // use this function when you want to assure that a switch-statement is exhaustive at compile-time 
 export function exhaustiveSwitch(p: never): never;
 export function exhaustiveSwitch(p: any): void {
