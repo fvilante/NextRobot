6b49cb5c10a541695e47693d5fd9cba3379853d1
Flavio <fvilante1@gmail.com>
2019-07-29 15:50:38 -0300

    initial implementation of a wave conversor

--- packages/driver-posijet2/src/driver-def-CMPP00LG.ts
+++ packages/driver-posijet2/src/driver-def-CMPP00LG.ts
@@ -9,23 +9,46 @@ import {
 } from './application-types'
 
 import { Memmap } from './driver-core'
-import { Waver } from './wave-core'
-import { Device } from './device-core';
+import { Waver, ToWave, FromWave, wave } from './wave-core'
+import { exhaustiveSwitch } from './utils'
+
+// tslint:disable: no-if-statement
+// tslint:disable: no-expression-statement
+
+
+
 
 export type Drive = {
     readonly 'Posicao Inicial': { readonly type: Space }
 }
 
+
+const spaceToWave: ToWave<Drive, Space> = (space, device) => {
+
+    const convertMilimeterToPulse = (_: number):number => _*777 //fix: not implement
+
+    switch(space.kind) {
+        case 'Pulse': return wave(space.value)
+        case 'Milimeter': return wave(convertMilimeterToPulse(space.value)) 
+        default:
+            return exhaustiveSwitch(space)
+    }
+
+}
+
+const waveToSpace: FromWave<Drive, Space> = (wave, device) => {
+    return Pulse(wave.waved)
+}
+
 //temp waver
-const spaceWaver: Waver<Drive,Space> = {
-    toWave: (space, device) => ({ waved: 12 }),
-    fromWave: (wave, device) => Milimeter(20) 
+const spaceWaver: Waver<Drive, Space> = {
+    toWave: spaceToWave,
+    fromWave: waveToSpace
 }
 
 export const MyMemmap1 : Memmap<Drive> = {
     'Posicao Inicial': { startWord: 0x60, startBit: 0, bitSize: 16, waver: spaceWaver},
 }
-
 
 
 
--- packages/driver-posijet2/src/wave-core.ts
+++ packages/driver-posijet2/src/wave-core.ts
@@ -4,10 +4,20 @@ import { AnyDriver, GetParameterType, GetParameters } from "./driver-core";
 
 // waved is a data that is agnostic to any device, programming language, operational system, etc.
 // normaly a binary data 
-export type Wave = { readonly waved: unknown}
+export type Wave = { readonly waved: number}
 
+
+export type ToWave<Drive extends AnyDriver, ParameterType> = 
+    (value: ParameterType, device: Device<Drive>) => Wave
+
+export type FromWave<Drive extends AnyDriver, ParameterType> = 
+    (value: Wave, device: Device<Drive>) => ParameterType
 
 export type Waver<Drive extends AnyDriver, ParameterType> = {
-    readonly toWave:  (value: ParameterType, device: Device<Drive>) => Wave
-    readonly fromWave: (value: Wave, device: Device<Drive>) => ParameterType
+    readonly toWave:  ToWave<Drive, ParameterType>
+    readonly fromWave: FromWave<Drive, ParameterType>
 }
+
+// Use this constructor to create a waved data
+type WaveConstructor = (_: number) => Wave
+export const wave: WaveConstructor = waved => ({waved})
