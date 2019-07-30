7857e41901b2af40f9eec60d93afe1470b193bc4
Flavio <fvilante1@gmail.com>
2019-07-30 07:56:37 -0300

    initial setup of kinematics package

---
+++ packages/kinematics/src/index.ts
@@ -0,0 +1,11 @@
+// example
+
+export const addOne = (a: number):number => a+1
+
+const main = () => {
+
+    console.log('hello world')
+
+}
+
+main()
---
+++ packages/kinematics/src/vector.ts
@@ -0,0 +1,9 @@
+
+
+export type Vector = {
+    x: number
+    y: number
+    z: number
+}
+
+export const Vector = (x:number,y:number,z:number): Vector => ({x,y,z}) 
