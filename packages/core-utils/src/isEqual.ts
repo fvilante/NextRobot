9c04c2c35602a958f4f24333d31e23fa498684fa
Flavio <fvilante1@gmail.com>
2019-08-12 06:42:20 -0300

    added temporarily Ramda.Equals to core-utils

--- packages/core-utils/src/isEqual.ts
+++ packages/core-utils/src/isEqual.ts
@@ -1,6 +1,11 @@
 import { flattenDeep } from './ArrayFlattenDeep'
+import { equals} from 'ramda'
 
-export const isArrayEqual = <T>(a1: readonly T[], a2: readonly T[]):boolean => 
+// fix: this function is not working (using ramda instead temporarily)
+const __isArrayEqual = <T>(a1: readonly T[], a2: readonly T[]):boolean => 
     // todo: can be optimized: using map in place of a reducer avoid early return
     flattenDeep(a1.map( val1 => a2.map( val2 => val1===val2))).every( test => test===true )
+
+
+export const isArrayEqual = <T>(a1: readonly T[], a2: readonly T[]):boolean => equals(a1,a2) 
 
