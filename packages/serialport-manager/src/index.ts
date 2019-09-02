015e6d0637deb1be36595c8c83b33f563de34571
fvilante <fvilante1@gmail.com>
2019-09-02 16:21:13 -0300

    added index.ts to serialport-manager package

---
+++ packages/serialport-manager/src/index.ts
@@ -0,0 +1,4 @@
+export { PortReference } from './port-opener/port-reference'
+export { serialPortOpenner as serialPortOpenner_PC } from './port-opener/port-opener-PC'
+export { Datalinker } from './core/datalinker'
+export { syncTransactioner } from './sync-transactioner/sync-transactioner'
