d17fcba95d6aaa170348b94b423ff7409a525473
fvilante <fvilante1@gmail.com>
2019-09-18 15:00:15 -0300

    bugfix in measure.ts

--- packages/driver-posijet2/src/memmap/kinematics/measure.ts
+++ packages/driver-posijet2/src/memmap/kinematics/measure.ts
@@ -86,42 +86,60 @@ const Measure = <
 
 // TYPES ---------------------------------------------------------------------------
 
-type ADIMENSIONAL = Measure<'0','0','0','0','m','min','rad','kg'>
+export type ADIMENSIONAL = Measure<'0','0','0','0','m','min','rad','kg'>
 
-type LENGTH<L extends   TypeUnits['Space']> =   Measure<'1','0','0','0',L,'min','rad','kg'>
-type TIME<T extends     TypeUnits['Time']>  =   Measure<'0','1','0','0','m',T,'rad','kg'>
-type ANGLE<A extends    TypeUnits['Angle']> =   Measure<'0','0','1','0','m','min',A,'kg'>
-type MASS<M extends     TypeUnits['Mass']>  =   Measure<'0','0','0','1','m','min','rad',M>
+export type LENGTH<L extends   TypeUnits['Space']> =   Measure<'1','0','0','0',L,'min','rad','kg'>
+export type TIME<T extends     TypeUnits['Time']>  =   Measure<'0','1','0','0','m',T,'rad','kg'>
+export type ANGLE<A extends    TypeUnits['Angle']> =   Measure<'0','0','1','0','m','min',A,'kg'>
+export type MASS<M extends     TypeUnits['Mass']>  =   Measure<'0','0','0','1','m','min','rad',M>
 
-type LINEARVELOCITY<        L extends TypeUnits['Space'], T extends TypeUnits['Time']> = Measure<'1','-1','0','0',L,T,'rad','kg'>
-type LINEARACCELERATION<    L extends TypeUnits['Space'], T extends TypeUnits['Time']> = Measure<'1','-2','0','0',L,T,'rad','kg'>
+export type LINEARVELOCITY<        L extends TypeUnits['Space'], T extends TypeUnits['Time']> = Measure<'1','-1','0','0',L,T,'rad','kg'>
+export type LINEARACCELERATION<    L extends TypeUnits['Space'], T extends TypeUnits['Time']> = Measure<'1','-2','0','0',L,T,'rad','kg'>
 
-type ANGULARVELOCITY<       A extends TypeUnits['Angle'], T extends TypeUnits['Time']> = Measure<'0','-1','1','0','m',T,A,'kg'>
-type ANGULARACCELERATION<   A extends TypeUnits['Angle'], T extends TypeUnits['Time']> = Measure<'0','-2','1','0','m',T,A,'kg'>
+export type ANGULARVELOCITY<       A extends TypeUnits['Angle'], T extends TypeUnits['Time']> = Measure<'0','-1','1','0','m',T,A,'kg'>
+export type ANGULARACCELERATION<   A extends TypeUnits['Angle'], T extends TypeUnits['Time']> = Measure<'0','-2','1','0','m',T,A,'kg'>
 
 
 
 // TYPE CONSTRUCTORS --------------------------------------------------------------- 
 
 
-const ADIMENSIONAL = (scalar: number): ADIMENSIONAL => Measure(scalar, DIMENSION('0','0','0','0'), UNIT('m', 'min', 'rad','kg'))
+export const ADIMENSIONAL = (scalar: number): ADIMENSIONAL => Measure(scalar, DIMENSION('0','0','0','0'), UNIT('m', 'min', 'rad','kg'))
 
-const LENGTH =  <L extends TypeUnits['Space']>  (scalar: number, lengthUnit: L): LENGTH<L> =>   Measure(scalar, DIMENSION('1','0','0','0'), UNIT(lengthUnit, 'min',  'rad','kg'))
-const TIME =    <T extends TypeUnits['Time']>   (scalar: number, timeUnit: T): TIME<T> =>       Measure(scalar, DIMENSION('0','1','0','0'), UNIT('m', timeUnit, 'rad','kg'))
-const ANGLE =   <A extends TypeUnits['Angle']>  (scalar: number, angleUnit: A): ANGLE<A> =>     Measure(scalar, DIMENSION('0','0','1','0'), UNIT('m', 'min', angleUnit,'kg'))
-const MASS =    <M extends TypeUnits['Mass']>   (scalar: number, massUnit: M): MASS<M> =>       Measure(scalar, DIMENSION('0','0','0','1'), UNIT('m', 'min', 'rad', massUnit))
+export const LENGTH =  <L extends TypeUnits['Space']>  (scalar: number, lengthUnit: L): LENGTH<L> =>   Measure(scalar, DIMENSION('1','0','0','0'), UNIT(lengthUnit, 'min',  'rad','kg'))
+export const TIME =    <T extends TypeUnits['Time']>   (scalar: number, timeUnit: T): TIME<T> =>       Measure(scalar, DIMENSION('0','1','0','0'), UNIT('m', timeUnit, 'rad','kg'))
+export const ANGLE =   <A extends TypeUnits['Angle']>  (scalar: number, angleUnit: A): ANGLE<A> =>     Measure(scalar, DIMENSION('0','0','1','0'), UNIT('m', 'min', angleUnit,'kg'))
+export const MASS =    <M extends TypeUnits['Mass']>   (scalar: number, massUnit: M): MASS<M> =>       Measure(scalar, DIMENSION('0','0','0','1'), UNIT('m', 'min', 'rad', massUnit))
 
-const LINEARVELOCITY = <L extends TypeUnits['Space'], T extends TypeUnits['Time']>(scalar: number, lengthUnit: L, timeUnit: T): LINEARVELOCITY<L,T> => Measure(scalar, DIMENSION('1','-1','0','0'), UNIT(lengthUnit, timeUnit, 'rad', 'kg'))
-const LINEARACCELERATION = <L extends TypeUnits['Space'], T extends TypeUnits['Time']>(scalar: number, lengthUnit: L, timeUnit: T): LINEARACCELERATION<L,T> => Measure(scalar, DIMENSION('1','-2','0','0'), UNIT(lengthUnit, timeUnit, 'rad', 'kg'))
+export const LINEARVELOCITY = <L extends TypeUnits['Space'], T extends TypeUnits['Time']>(scalar: number, lengthUnit: L, timeUnit: T): LINEARVELOCITY<L,T> => Measure(scalar, DIMENSION('1','-1','0','0'), UNIT(lengthUnit, timeUnit, 'rad', 'kg'))
+export const LINEARACCELERATION = <L extends TypeUnits['Space'], T extends TypeUnits['Time']>(scalar: number, lengthUnit: L, timeUnit: T): LINEARACCELERATION<L,T> => Measure(scalar, DIMENSION('1','-2','0','0'), UNIT(lengthUnit, timeUnit, 'rad', 'kg'))
 
-const ANGULARVELOCITY = <A extends TypeUnits['Angle'], T extends TypeUnits['Time']>(scalar: number, angleUnit: A, timeUnit: T): ANGULARVELOCITY<A,T> => Measure(scalar, DIMENSION('0','-1','1','0'), UNIT('m', timeUnit, angleUnit, 'kg'))
-const ANGULARACCELERATION = <A extends TypeUnits['Angle'], T extends TypeUnits['Time']>(scalar: number, angleUnit: A, timeUnit: T): ANGULARACCELERATION<A,T> => Measure(scalar, DIMENSION('0','-2','1','0'), UNIT('m', timeUnit, angleUnit, 'kg'))
+export const ANGULARVELOCITY = <A extends TypeUnits['Angle'], T extends TypeUnits['Time']>(scalar: number, angleUnit: A, timeUnit: T): ANGULARVELOCITY<A,T> => Measure(scalar, DIMENSION('0','-1','1','0'), UNIT('m', timeUnit, angleUnit, 'kg'))
+export const ANGULARACCELERATION = <A extends TypeUnits['Angle'], T extends TypeUnits['Time']>(scalar: number, angleUnit: A, timeUnit: T): ANGULARACCELERATION<A,T> => Measure(scalar, DIMENSION('0','-2','1','0'), UNIT('m', timeUnit, angleUnit, 'kg'))
+
+
+// sumary
+
+
+export const Kinematic = {
+    ADIMENSIONAL,
+    LENGTH,
+    TIME,
+    ANGLE,
+    MASS,
+    LINEARVELOCITY,
+    LINEARACCELERATION,
+    ANGULARVELOCITY,
+    ANGULARACCELERATION,
+}
+
+
 
 
 // MEASURE ADDITION OPERATIONS --------------------------------------------------------------- 
 
 
-const sum = <
+export const sum = <
     L extends EXP, 
     T extends EXP, 
     A extends EXP, 
@@ -136,7 +154,7 @@ const sum = <
 
 
 /** subtracts b from a */
-const sub = <
+export const sub = <
     L extends EXP, 
     T extends EXP, 
     A extends EXP, 
@@ -153,20 +171,23 @@ const sub = <
 
 // MEASURE MULTIPLICATION OPERATIONS --------------------------------------------------------------- 
 
-const mul = <
+export const mul = <
     L0 extends EXP, 
     T0 extends EXP, 
     A0 extends EXP, 
     M0 extends EXP,
-    L1 extends EXP, 
-    T1 extends EXP, 
-    A1 extends EXP, 
-    M1 extends EXP,
     LU extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
     TU extends TypeUnits['Time'],
     AU extends TypeUnits['Angle'],
     MU extends TypeUnits['Mass'], 
-     >(a: Measure<L0,T0,A0,M0,LU,TU,AU,MU>, b: Measure<L1,T1,A1,M1,LU,TU,AU,MU>) => {
+    >(a: Measure<L0,T0,A0,M0,LU,TU,AU,MU>) => 
+     
+    <
+    L1 extends EXP, 
+    T1 extends EXP, 
+    A1 extends EXP, 
+    M1 extends EXP
+    > (b: Measure<L1,T1,A1,M1,LU,TU,AU,MU>) => {
         
         const newScalar = a.scalar * b.scalar
         const newDimension = dimensionAdder(a.dimension, b.dimension)
@@ -178,21 +199,26 @@ const mul = <
 
 
 /** divides a by b*/
-const div = <
+export const div = <
     L0 extends EXP, 
     T0 extends EXP, 
     A0 extends EXP, 
     M0 extends EXP,
-    L1 extends EXP, 
-    T1 extends EXP, 
-    A1 extends EXP, 
-    M1 extends EXP,
     LU extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
     TU extends TypeUnits['Time'],
     AU extends TypeUnits['Angle'],
     MU extends TypeUnits['Mass'], 
-      >(a: Measure<L0,T0,A0,M0,LU,TU,AU,MU>, b: Measure<L1,T1,A1,M1,LU,TU,AU,MU>) => {
+    >(a: Measure<L0,T0,A0,M0,LU,TU,AU,MU>) => 
+      
+    <
+    L1 extends EXP, 
+    T1 extends EXP, 
+    A1 extends EXP, 
+    M1 extends EXP,
+    >(b: Measure<L1,T1,A1,M1,LU,TU,AU,MU>) => {
+
           return Measure(a.scalar / b.scalar, dimensionSubtracter(a.dimension, b.dimension), a.unit)
+          
        }
 
 
@@ -201,7 +227,7 @@ const div = <
 
 
 
-const scalarMul = <
+export const scalarMul = <
     L extends EXP, 
     T extends EXP, 
     A extends EXP, 
@@ -215,23 +241,23 @@ const scalarMul = <
      }
 
 
-const scalarDiv = <
-     L extends EXP, 
-     T extends EXP, 
-     A extends EXP, 
-     M extends EXP,
-     LU extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
-     TU extends TypeUnits['Time'],
-     AU extends TypeUnits['Angle'],
-     MU extends TypeUnits['Mass'], 
-     >(scalar: number, o: Measure<L,T,A,M,LU,TU,AU,MU>): Measure<L,T,A,M,LU,TU,AU,MU> => {
+export const scalarDiv = <
+    L extends EXP, 
+    T extends EXP, 
+    A extends EXP, 
+    M extends EXP,
+    LU extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
+    TU extends TypeUnits['Time'],
+    AU extends TypeUnits['Angle'],
+    MU extends TypeUnits['Mass'], 
+    >(scalar: number, o: Measure<L,T,A,M,LU,TU,AU,MU>): Measure<L,T,A,M,LU,TU,AU,MU> => {
          return Measure(o.scalar / scalar, o.dimension, o.unit)
-      }
+    }
  
 
 // MEASURE MULTIPLICATION BY SCALAR OPERATIONS --------------------------------------------------------------- 
 
-type UnitConversorArgument<
+export type UnitSystem<
     L extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
     T extends TypeUnits['Time'],
     A extends TypeUnits['Angle'],
@@ -244,12 +270,13 @@ type UnitConversorArgument<
 
 }
 
-const UnitConversorArgument = <
+/** Define all primitive units */
+export const UnitSystem = <
     L extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
     T extends TypeUnits['Time'],
     A extends TypeUnits['Angle'],
     M extends TypeUnits['Mass'],    
-    >( lengthUnit: L, timeUnit: T, angleUnit: A, massUnit: M): UnitConversorArgument<L,T,A,M> => ({
+    >( lengthUnit: L, timeUnit: T, angleUnit: A, massUnit: M): UnitSystem<L,T,A,M> => ({
 
         length: lengthUnit, 
         time: timeUnit, 
@@ -258,7 +285,24 @@ const UnitConversorArgument = <
 
     })
 
-const BASEUNIT: UnitConversorArgument<'m','min','rad','kg'> = {
+/** helper to client of UnitConversor */
+export const getUnits = <
+    L extends EXP, 
+    T extends EXP, 
+    A extends EXP, 
+    M extends EXP,
+    LU extends TypeUnits['Space'], //todo: change name convention: from 'Space' to 'Length'
+    TU extends TypeUnits['Time'],
+    AU extends TypeUnits['Angle'],
+    MU extends TypeUnits['Mass'], 
+    >(o: Measure<L,T,A,M,LU,TU,AU,MU>): UnitSystem<LU,TU,AU,MU> => {
+        return UnitSystem(o.unit.length.value.type, o.unit.time.value.type, o.unit.angle.value.type, o.unit.mass.value.type)
+    }
+
+
+
+/** Unit System: International System */
+export const InternationalSystem: UnitSystem<'m','min','rad','kg'> = {
     length: 'm',
     time: 'min',
     angle: 'rad',
@@ -266,7 +310,7 @@ const BASEUNIT: UnitConversorArgument<'m','min','rad','kg'> = {
 }
 
 
-const UnitConversor = <   
+export const UnitConversor = <   
     L extends EXP, 
     T extends EXP, 
     A extends EXP, 
@@ -279,7 +323,7 @@ const UnitConversor = <
     TU1 extends TypeUnits['Time'],
     AU1 extends TypeUnits['Angle'],
     MU1 extends TypeUnits['Mass']> 
-    (m: Measure<L,T,A,M,LU0,TU0,AU0,MU0>, newUnit: UnitConversorArgument<LU1,TU1,AU1,MU1>): Measure<L,T,A,M,LU1,TU1,AU1,MU1> => {
+    (m: Measure<L,T,A,M,LU0,TU0,AU0,MU0>, newUnit: UnitSystem<LU1,TU1,AU1,MU1>): Measure<L,T,A,M,LU1,TU1,AU1,MU1> => {
 
 
         const length = __UnitConversor(m.unit.length, newUnit.length)
@@ -308,10 +352,25 @@ const UnitConversor = <
 
         return Measure(_newScalar, m.dimension, _newUnit)
 
-    }
+}
+
+export const KinematicOperation = {
+    sum,
+    sub,
+    mul,
+    div,
+    scalarMul,
+    scalarDiv,
+    //UnitConversorArgument,
+    BASEUNIT: InternationalSystem,
+    UnitConversor,
+    getUnits
+}
 
 
 
+
+// informal test
 
 
 const Test = () => {
@@ -329,16 +388,16 @@ const Test = () => {
     const a = sum(t0)(t1)
     //const b = sum(t0,s0) // OK -> error in type of s0
     
-    const s0_ = UnitConversor(s0, {...BASEUNIT, length: 'mm'})
+    const s0_ = UnitConversor(s0, {...InternationalSystem, length: 'mm'})
 
     const delta_t = sub(t1)(t0)
-    const delta_s = sub(s1)(s0_)
-    const velocity = div(delta_s, delta_t) 
+    const delta_s = UnitConversor(sub(s1)(s0_), {...InternationalSystem})
+    const velocity = div(delta_s)(delta_t) 
 
     console.table(velocity) // Measure<"1", "-1", "0", "0">
 
-    const acceleration = div(velocity, TIME(1, 'min'))
-    const acceleration2 = div(delta_s, mul(TIME(1, 'min'), TIME(1, 'min')))
+    const acceleration = div(velocity)(TIME(1, 'min'))
+    const acceleration2 = div(delta_s)(mul(TIME(1, 'min'), TIME(1, 'min')))
 
     const r = sum(acceleration)(acceleration2)
 
@@ -361,7 +420,7 @@ const Test = () => {
     const tt0 = TIME(10, 'sec')
     const tt1 = TIME(2, 'min')
 
-    const tt1_ = UnitConversor(tt1, {...BASEUNIT, time: 'sec'})
+    const tt1_ = UnitConversor(tt1, {...getUnits(tt1), time: 'sec'})
 
     const summed = sum(tt0)(tt1_)
 
@@ -404,11 +463,29 @@ const Test2 = () => {
     const tt0 = TIME(10, 'sec')
     const tt1 = TIME(2, 'min')
 
-    const tt1_ = UnitConversor(tt1, {...BASEUNIT, time: 'sec'})
+    const tt1_ = UnitConversor(tt1, {...InternationalSystem, time: 'sec'})
 
     console.log(tt1)
     console.log(tt1_)
 }
+
+const Test3 = () => {
+
+
+    const v0 = LINEARVELOCITY(10,'inch','sec')
+    const v1 = UnitConversor(Kinematic.LINEARVELOCITY(10,'mm', 'sec'), {...getUnits(v0), length: 'inch'})
+    const delta_v = sub(v1)(v0)
+
+    const t0 = TIME(0, 'sec')
+    const t1 = TIME(10, 'sec')
+    const delta_t = sub(t1)(t0)
+    
+    const delta_t_ = UnitConversor(delta_t, UnitSystem("inch", "sec", "rad", "kg"))
+
+    const acceleration = div(delta_v)(delta_t_)
+    
+}
+
 
 // tslint:disable-next-line: no-expression-statement
 Test2()
