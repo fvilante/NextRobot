97b72e112cf964d5b907f8a0553df7b2cb271e08
Flavio <fvilante1@gmail.com>
2019-10-10 12:42:36 -0300

    cmpp TransportService now process multiple works and give an intermediary callback for percentage (progressbar) information.

--- packages/driver-posijet2/src/transport-layer/transport-service.ts
+++ packages/driver-posijet2/src/transport-layer/transport-service.ts
@@ -1,10 +1,17 @@
 
-import { TransactResult } from './transaction/TransactResult'
-import { Reader } from '@nextrobot/core-utils'
+import { TransactResult, AnyTransactResult } from './transaction/TransactResult'
+import { Reader, range, rangeSync, foldLeftArray } from '@nextrobot/core-utils'
 import { Env } from './transaction/Env'
 import { AnyDirecao } from './transaction/pacote-models/base-model/Direcao'
 import { transact } from './transaction/transact'
 import { PacoteDeTransmissao } from './transaction/pacote-models/PacoteDeTransmissao'
+import { serialPortOpenner_PC } from '@nextrobot/serialport-manager'
+import { CmppAddress } from './transaction/CmppAddress'
+import { generatorToList } from '@nextrobot/core-utils/lib/Generators/number-generator-sync'
+
+
+/** general use callback to inform intermediate data of loading progress  */
+type LoadBarCallback = (percentage:number) => void
 
 
 type WordAddress = number
@@ -15,7 +22,7 @@ type ServiceWork<D extends AnyDirecao> = Reader<Env, Promise<TransactResult<D>>>
 
 export type TransportService = {
 
-    readonly run: <D extends AnyDirecao>(w: ServiceWork<D>, env: Env) => Promise<TransactResult<D>>
+    readonly run: <D extends AnyDirecao, T extends TransactResult<D>>(w: readonly ServiceWork<D>[], env: Env,  loading?: LoadBarCallback) => Promise<readonly TransactResult<D>[]>
     
     //lazy (pure) operations
 
@@ -29,14 +36,37 @@ export type TransportService = {
 
 }
 
+
+// =============== implementations ==================================================================
+
 //helpers
 const runTransaction_Helper = <D extends AnyDirecao>(env: Env, pacote: PacoteDeTransmissao<D>) => transact(pacote).run(env)
 const makeErrorMsg = <E extends Error>(err: E) => `Erro no service Cmpp de transport-layer. Detalhe: ${err}`
 
 
-// ----- implementations ------------
+const run: TransportService['run']  = async <D extends AnyDirecao>(works: readonly ServiceWork<D>[], env: Env, loading?: LoadBarCallback): Promise<readonly TransactResult<D>[]> => {
 
-const run: TransportService['run'] = (work, env) => work.run(env)
+    // tslint:disable: no-let no-expression-statement no-if-statement
+    let result: TransactResult<D>[] = []
+
+    let count: number = works.length
+
+    for (const work of works) {
+
+        // run
+        const r = await work.run(env)
+        result = [...result, r]
+
+        // inform
+        if (loading) { 
+            const concluded_percentage = Math.round((((count-works.length)*(-1))/works.length)*100)
+            loading( concluded_percentage)
+        }
+        count = count - 1
+    }
+
+    return result
+}
 
 const Set16BitsValue: TransportService['Set16BitsValue'] = (waddr, value) => Reader( env => { 
 
@@ -106,3 +136,66 @@ export const GetTransportService = ():TransportService => ({
     Reset16BitsBitmask,
 
 })
+
+
+
+// -------------- informal test -------------------
+
+const service = GetTransportService()
+
+const env: Env = {
+    portOpener: serialPortOpenner_PC,
+    cmppAddress: CmppAddress({baudRate: 9600, channel: 0, portName: 'COM6'})
+}
+
+const Test1 = async () => {
+
+    //send just of transacion
+
+    const e1 = service.Get16BitsValue(0x70)
+
+    const a = e1.run(env)
+
+    console.log(await a) //ok
+
+}
+
+
+const Test2 = async () => {
+
+    //little batch of transacions
+
+    const e1 = service.Get16BitsValue(0x70)
+    const e2 = service.Set16BitsValue(0x70, 10)
+    const e3 = service.Get16BitsValue(0x70)
+
+    const r1 = await service.run([e1], env)
+    const r2 = await service.run([e2], env)
+    const r3 = await service.run([e3], env)
+        
+
+    console.log(r1, r2, r3)
+
+}
+
+const Test3 = async () => {
+
+    //big batch of transacions
+
+    const addrs = foldLeftArray( generatorToList(rangeSync(0,0xff)), [] as number[], (acc, cur) => {
+        const black_list_addresses:number[] = [58,217,224,231] //[58, 217, 224, 231]
+        return black_list_addresses.includes(cur) ? [...acc] : [...acc, cur]
+    })
+
+    const works = addrs.map( addr => service.Get16BitsValue(addr))
+
+    const r = await service.run(works, env, porcentagem => console.log(`porcentagem ${porcentagem}%`))
+
+    console.log(`Resultdo: `)
+    const b = r.map( x => console.log(x) )
+
+}
+
+
+// tslint:disable-next-line: no-expression-statement
+Test3()
