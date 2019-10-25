b6c15db2ef6f2498429d5c433a149b1a1b014b4b
Flavio <fvilante1@gmail.com>
2019-10-25 15:39:24 -0300

    PortOpenerPC addapted to new conventions: - Use Future instead of Promises - Added new metadata do PortOpenner type - Added informal unit test

--- packages/serialport-manager/src/effects/port-opener-PC.ts
+++ packages/serialport-manager/src/effects/port-opener-PC.ts
@@ -10,7 +10,9 @@ import * as Stream from 'stream'
 const a = Stream.Readable
 
 import * as serial_port from 'serialport';
-import { _Port } from '../data-models/port';
+import { _Port, PortReference, defaultPortConfig } from '../data-models/port';
+import { detectSerialPorts } from './detectSerialPorts';
+import { Future, Right, Left, delay } from '@nextrobot/core-utils';
 type SerialPort = unknown //typeof serial_port.default
 const SerialPort = serial_port.default
 
@@ -27,6 +29,24 @@ const NodeBufferToBytes = (buffer: any): Bytes => {
     }
     return Bytes(bytes)
 }
+
+const __getPortInfo_PC = (port: _Port['Name']): Future<_Port['Info']> => Future( resolver => {
+    const f = async () => {
+        const portInfos = await detectSerialPorts()
+        await delay(250).runP()
+        const hasFound = portInfos.find( _port => _port.name === port )
+        if (hasFound)
+            return hasFound
+            throw new Error(`Cannot find port information for serial port number ${port}`) 
+
+    }
+    f().then( info => resolver(Right( info )))
+})
+ 
+
+        
+
+    
 
 
 // === implementation ====
@@ -45,46 +65,86 @@ const NodeBufferToBytes = (buffer: any): Bytes => {
  * @param portName 
  * @param portConfig 
  */
-export const serialPortOpenner: _Port['Opener'] = (port: _Port['Reference']): Promise<_Port['Openned']> => {
+export const serialPortOpenner: _Port['Opener'] = port => {
 
-    return new Promise( async ( resolve, reject ) => {
+    return Future( resolver => {
+
+            //const wait_to_open = delay(250)
 
             // create concrete port
-            const aPort = new SerialPort(port.name, {...port.config, baudRate: port.baudRate, ...{ autoOpen: false }})
+            const getAPort = () => new SerialPort(port.name, {...port.config, baudRate: port.baudRate, ...{ autoOpen: false }})
             
+            const aPort = getAPort()
 
             // open handler
-            const onOpen = ():void => {
-                
-                // port interface
-                const openedPort: _Port['Openned'] = {
-                    write: data => aPort.write([...data.bytes]),
-                    close: () => { aPort.close() },
-                    onClose: callback => { aPort.on('close', callback) },
-                    onData: callback => aPort.on('data', (data: unknown) => callback(NodeBufferToBytes(data))),
-                    onError: callback => aPort.on('error', callback),
-                    // todo: add onWrite 
-                }
-                resolve(openedPort)
-            }
+            const createPort = ():_Port['Openned'] => {
+                        return {
+                            write: data => aPort.write([...data.bytes]),
+                            close: () => { aPort.close() },
+                            onClose: callback => { aPort.on('close', callback) },
+                            onData: callback => aPort.on('data', (data: unknown) => callback(NodeBufferToBytes(data))),
+                            onError: callback => aPort.on('error', callback),
+                            info: () => ({
+                                name: 'loopback-test-mockup-port',
+                                detail: {kind: 'RemotePortInfo'}
+                            }),//portInfo,
+                            reference: () => port,
+                            // todo: add onWrite
+                        }
+                    }
+
+
 
             // error handler for port opening
             const openError = (err?: Error | null): void => 
                 (err === undefined || err === null)
-                    ? reject(new Error(`Error: Cannot open serial port ${port.name}`))
-                    : reject(err)
+                    ? resolver( Left( new Error(`Error: Cannot open serial port ${port.name}`) ) )
+                    : resolver( Left( err ) )
             
 
             // set handlers and open
-            aPort.on('open', onOpen)
+            aPort.on('open', () => resolver(Right(createPort())))
             aPort.on('error', openError) //todo: check if 'openError' is as expected overwritted when iPort.onError is called
             
             // do try to open
             aPort.open() 
             
-        
+          
+
+
         })
         
         
 }
+
+// open and just close the port
+const Test1 = async () => {
+
+    const p = PortReference( 'COM6', 2400, defaultPortConfig )
+
+    const opened = serialPortOpenner(p)
+
+    const closeImediately = opened.fmap( o => Future<undefined>( resolver => {
+
+        o.onError( err => { console.log( `Erro -> ${err}`)})
+        o.onClose( () => { console.log(`Porta sendo fechada`); resolver(Right(undefined)) })
+        o.onData( b => { console.log(`recebendo ${b.bytes}`) })
+        o.write( Bytes([0x1B, 0x02, 0xC2, 0x50, 0x61, 0x02, 0x1B, 0x03, 0x86]))
+        delay(1000).runP().then( () => o.close())
+
+
+    }))
+
+    const b = closeImediately.runP().then( ei => ei.match({
+        Left: err => {console.log(`final com erro -> ${err}`)},
+        Right: a => { console.log(`final bem sucedido ${a}`)}
+
+    }))
+
+
+
+}
+
+
+//Test1()
 
