c7f8ad572fc38d4f8f7e6390a5cfe5864b51bdef
Flavio <fvilante1@gmail.com>
2019-11-26 08:51:06 -0200

    Try return type changed from 'Either' to 'Result' monad.

--- packages/core-utils/src/monads/try.ts
+++ packages/core-utils/src/monads/try.ts
@@ -1,36 +1,36 @@
-import { Either, Right, Left } from "./either";
+import { Result } from "./result";
 
 
 /** Try execute an unsafeEffect and catches the error into the left side of either
  * Note: Eager evaluation
  */
-export const Try = <A>(LazyUnsafeEffect: () => A): Either<Error,A> => {
+export const Try = <A>(LazyUnsafeEffect: () => A): Result<Error,A> => {
 
     try { 
-        return Right( LazyUnsafeEffect() ) 
+        return Result.Value( LazyUnsafeEffect() ) 
     } catch(e) {
         // tslint:disable: no-if-statement
         if(e instanceof Error) {
             // properly handle Error e
-            return Left(e) 
+            return Result.Error(e) 
         }
         else if(typeof e === 'string' || e instanceof String) {
             // properly handle e or...stop using libraries that throw naked strings
-            return Left(new Error(String(e))) 
+            return Result.Error(new Error(String(e))) 
         }
         else if(typeof e === 'number' || e instanceof Number) {
             // properly handle e or...stop using libraries that throw naked numbers
-            return Left(new Error(String(e)))
+            return Result.Error(new Error(String(e)))
         }
         else if(typeof e === 'boolean' || e instanceof Boolean) {
             // properly handle e or...stop using libraries that throw naked booleans
-            return Left(new Error(String(e)))
+            return Result.Error(new Error(String(e)))
         }
         else {
             // if we can't figure out what what we are dealing with then
             // probably cannot recover...therefore, rethrow
             // Note to Self: Rethink my life choices and choose better libraries to use.
-            return Left(new Error(String(e)))
+            return Result.Error(new Error(String(e)))
         }
         // tslint:enable: no-if-statement
     }
@@ -42,25 +42,25 @@ export const Try = <A>(LazyUnsafeEffect: () => A): Either<Error,A> => {
 
 const Test = () => {
 
-    type AnyEither = Either<any,any>
+    type AnyResult = Result<any,any>
 
-    const DoThrows = () => {
+    const LetsThrow = () => {
         throw new Error(`I've been throwed an error!`)
         return 2
     }
 
-    const NoThrow = () => {
+    const NeverThrow = () => {
         return `hello world`
     }
 
-    const a = Try(DoThrows) // Ok a is type: Either<Error, number>
-    const b = Try(NoThrow)  // ok b is type: Either<Error, string>
+    const a = Try(LetsThrow) // Ok a is type: Result<Error, number>
+    const b = Try(NeverThrow)  // ok b is type: Result<Error, string>
 
-    const showResult = <T extends AnyEither>(e: T):string => {
-        return e.match({
-            Left:   err => `Peguei o erro: "${err.message}"`,
-            Right:  val => `Valor chegou: "${val}"`
-        })
+    const showResult = <T extends AnyResult>(data: T):string => {
+        return data.fold(
+            err => `Peguei o erro: "${err.message}"`,
+            val => `Valor chegou: "${val}"`
+        )
     }
 
     console.log(showResult(a))
