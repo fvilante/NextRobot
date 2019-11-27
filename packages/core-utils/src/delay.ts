ee2fe8553b70aeb66164c6d1b9e12aa876f590d3
Flavio <fvilante1@gmail.com>
2019-11-27 14:37:18 -0200

    return type of delay corrected

--- packages/core-utils/src/delay.ts
+++ packages/core-utils/src/delay.ts
@@ -1,6 +1,6 @@
 import { Future } from "./monads/future"
 
-export const delay = (ms:number):Future<undefined> =>
+export const delay = (ms: number): Future<void, undefined> =>
     Future( ok => {
         // tslint:disable-next-line: no-expression-statement
         setTimeout( () => ok(undefined), ms)
