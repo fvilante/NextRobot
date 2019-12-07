eed976ba0620b96e05e7c226fadfab8402a551ad
Flavio <fvilante1@gmail.com>
2019-12-07 07:41:45 -0200

    Refactored List<A>: inserted methods toPairs. Improved Eargerness description.

--- packages/core-utils/src/monads/list.ts
+++ packages/core-utils/src/monads/list.ts
@@ -1,27 +1,29 @@
 import { Maybe, Nothing, Just } from "./maybe"
 import { foldLeftArray } from "../array/foldLeftArray"
 import { flattenDeep } from "../array/ArrayFlattenDeep"
+import { Pair } from "./pair"
 
+type TKey = string | number | symbol    // todo: extract this type definition to a exclusive file 
 
 export type List<A> = {
     readonly kind: 'List'
+
+    readonly unsafeRun: () => readonly A[]
 
     readonly map: <B>(f: (_:A) => B) => List<B>
     readonly fmap: <B>(f: (_:A) => List<B>) => List<B>
     readonly foldLeft: <T>(initialValue: T, fn: (acc: T, cur: A) => T ) => T
     readonly pickByIndex: (index: number) => Maybe<A>
-    readonly _unsafePickByIndex: (index: number) => A //will throw if at index no parameter exists
     readonly length: () => number
-    readonly toArray: () => readonly A[]
     readonly every: (predicate: (_:A) => boolean ) => boolean
 
     readonly mapMaybe: <B>(f: (_:A) => Maybe<B>) => List<B> //do not transfer if 'nothing'
 
     readonly head: () => Maybe<A> 
 
+    readonly toPairs: () => List<Pair<number,A>> 
+
 }
-
-
 
 /** Similar to ReadonlyArray<T> but more type constrained and functional (but not lazy)
  * todo: make construction less dependent from JS array implementation. 
@@ -29,57 +31,78 @@ export type List<A> = {
  * ATTENTION: Eager evaluation here!*/
 export const List = <A>(arr: readonly A[]): List<A> => {
 
-    const map: List<A>['map'] = f => {
-        return List( arr.map(f) )
+    type T = List<A>
+
+    const unsafeRun: T['unsafeRun'] = () => {
+        return arr
     }
 
-    const fmap: List<A>['fmap'] = f => {
-        return List_.flatten( map(f) )
-    }
-
-    const foldLeft: List<A>['foldLeft'] = (initial, fn) => {
-        return foldLeftArray(arr, initial, fn )
-    }
-
-    const pickByIndex: List<A>['pickByIndex'] = index => {
+    const pickByIndex: T['pickByIndex'] = index => {
+        const arr = unsafeRun()
         const isOutOfRange = (arr.length-1) > index 
         return isOutOfRange ? Nothing<A>() : Just(arr[index])
     }
 
-    const every: List<A>['every'] = f => {
+    const map: T['map'] = f => {
+        const arr = unsafeRun()
+        return List( arr.map(f) )
+    }
+
+    const fmap: T['fmap'] = f => {
+        return List_.flatten( map(f) )
+    }
+
+    const foldLeft: T['foldLeft'] = (initial, fn) => {
+        const arr = unsafeRun()
+        return foldLeftArray(arr, initial, fn )
+    }
+
+    const every: T['every'] = f => {
+        const arr = unsafeRun()
         return arr.every(f)
     }
 
-    const _unsafePickByIndex: List<A>['_unsafePickByIndex'] = index => arr[index]
+    const length: T['length'] = () => {
+        const arr = unsafeRun()
+        return arr.length
+    }
 
-    const length: List<A>['length'] = () => arr.length
-
-    const toArray: List<A>['toArray'] = () => arr
-
-    const mapMaybe: List<A>['mapMaybe'] = f => {
+    const mapMaybe: T['mapMaybe'] = f => {
         type B = ReturnType<ReturnType<typeof f>['_fromJust']>
         const r = foldLeft([] as readonly B[], (acc, cur) => {
             const mb = f(cur)
-            return mb.isJust ? [...acc, mb._fromJust()] : acc
+            return mb.match({
+                Just: val => [...acc, val],
+                Nothing:     acc,
+            }) 
         })
         return List(r)
     }
 
-    const head: List<A>['head'] = () => List_.head(List(arr)) //Todo: can be optimized ? many wraps beeing performed here
+    const head: T['head'] = () => { 
+        return pickByIndex(0)
+    }
+
+    const toPairs: T['toPairs'] = () => {
+        const arr = unsafeRun()
+        const a = arr.map( (value, index) => Pair(index, value))
+        const b = List(a)
+        return b 
+    }
+
 
     return {
         kind: 'List',
+        unsafeRun,
         map,
         fmap,
         foldLeft,
         pickByIndex,
-        _unsafePickByIndex,
         length,
-        toArray,
         every,
         mapMaybe,
         head,
-
+        toPairs,
     }
 }
 
@@ -87,19 +110,21 @@ export const List = <A>(arr: readonly A[]): List<A> => {
 
 export type List_ = {
     readonly flatten: <A>(mma: List<List<A>>) => List<A>
-    readonly head: <A>(_: List<A>) => Maybe<A> // inspired in Elm language (Note: Check, don't think is it so performant)
+    readonly head: <A>(_: List<A>) => Maybe<A> // inspired in Elm language (Note: Check performance, don't think is it so performant)
 }
 
 
 const flatten: List_['flatten'] = mma => {
-    type A = ReturnType<ReturnType<typeof mma._unsafePickByIndex>['_unsafePickByIndex']>
-    const arrarr = mma.toArray().map( x => x.toArray())
-    const arr = flattenDeep<readonly A[],A>(arrarr)
-    return List(arr)
+    type List = ReturnType<(typeof mma)['unsafeRun']>[0]
+    type A = ReturnType<List['unsafeRun']>[0]
+    const arr = mma.unsafeRun()
+    const a = arr.map( x => x.unsafeRun())
+    const b = flattenDeep<readonly A[],A>(a)
+    return List(b)
 }
 
-const head: List_['head'] = li => { 
-    return li.pickByIndex(0)
+const head: List_['head'] = a => { 
+    return a.head()
 }
 
 export const List_: List_ = {
