b3b08a5bb4e5da921dfd0586ad14b90c8b2e5c0b
Flavio <fvilante1@gmail.com>
2019-12-07 05:05:26 -0200

    bugfixes and refactoring in zmanaged monad

--- packages/core-utils/src/monads/zio/zmanaged.ts
+++ packages/core-utils/src/monads/zio/zmanaged.ts
@@ -2,12 +2,13 @@ import { ZIO, ZIO_ } from "./zio";
 import { Either, Right, Left } from "../either";
 import values from "ramda/es/values";
 import { Result } from "../result";
+import { delay } from "../../delay";
 
 
 
 type ZManaged<R,E,A> = {
 
-    readonly uses: <R1,E1,B>(f: (resource: A) => ZIO<R1,E1,B>) =>  ZIO<R1,E1,B>
+    readonly uses: <B>(f: (resource: A) => ZIO<R,E,B>) =>  ZIO<R,E,B>
 
 }
 
@@ -17,31 +18,26 @@ type ZManaged<R,E,A> = {
  *  Reads an enviroment R and opens a resource A and call the function callback presented to uses combinator
  *  after run uses, execute the closer effect. If use call back throws, the resource is assured to be closed.
  */
-const ZManaged = <R,E,A>(aquire: ZIO<R,E,A>, release: ZIO<A,void,undefined>) => {
+const ZManaged = <R,E,A>(aquire: ZIO<R,E,A>, release: (_:A) => void): ZManaged<R,E,A> => {
 
     // helper
-    const _fmap = <R1 extends R0,E1,B,R0,E0 extends E1,A0>(m: ZIO<R0,E0,A0>, f: (_:A0) => ZIO<R1,E1,B>):ZIO<R1,E1,B> => {
+    const _fmap = <R1 extends R0,E1,B,R0,E0 extends E1,A0>(m: ZIO<R0,E0,A0>, f: (_:A0) => ZIO<R1,E1,B>): ZIO<R1,E1,B> => {
         const m0 = m.contramap( (_:R1) => _)
         const m1 = m0.mapError( (_:E0):E1 => _)
         const m2 = m1.fmap(f)
         return m2
     }
 
-    const uses = <B>(f: (resource: A) => ZIO<R,E,B>):ZIO<R,E,B> => {
-         //identity + side-effect to close
-        const useAndRelease = (resource: A) => ZIO( (env:R) => {
-            return f(resource).unsafeRunResult(env).match<Result<E, B>>(
-                _err => { release.unsafeRun(resource); return Result.Error(_err); },
-                _val => { release.unsafeRun(resource); return Result.Value(_val); },
-            )
-        } )
-        
-        //return aquire.fmap( useAndRelease)
-        return aquire.fmap(useAndRelease)
+    const uses: ZManaged<R,E,A>['uses'] = f => {
+        return aquire.fmap( a => {
+            return f(a).map( b => { 
+                release(a)
+                return b
+            })
+
+        })
     }
        
-
-
     return {
         uses,
     }
@@ -51,64 +47,67 @@ const ZManaged = <R,E,A>(aquire: ZIO<R,E,A>, release: ZIO<A,void,undefined>) => 
 
 // Informal Test
 
-const Test = () => {
+const Test = async () => {
 
-    console.log(`creating effects...`)
-
-    type PortId = { readonly id: string } 
-    const PortId = (id:string):PortId => ({id})
+    /** Plataform-Specific */
 
     type Resource = {
-        readonly kind: PortId
+        readonly kind: string
         readonly write?: undefined
         readonly read?: undefined
+        readonly close: () => void
     }
 
     type ResourceOpener = (port:string) => Resource
+    const ResourceOpener: ResourceOpener = port =>  { 
+        console.log(`Openning resorce: ${port}`)
+        return { kind: port, close: () => { console.log(`Closing resource: ${port}`)} }
+    }
 
-    const ResourceOpener: ResourceOpener = port => ({
-        kind: PortId(port)
-    })
+    /** Global Enviroment: Impure to Pure frontier */
 
     type ResourcesEnviroment = {
         readonly resourceOpener: ResourceOpener
-        readonly portIds: readonly PortId[]
+        readonly portIds: readonly string[]
     }
-
-    type GlobalEnviroment = ResourcesEnviroment & { readonly global: true}
 
     const ResourcesEnviroment: ResourcesEnviroment = {
         resourceOpener: ResourceOpener,
-        portIds: [PortId(`COM1`), PortId(`COM2`), PortId(`COM3`)]
+        portIds: [`COM1`,`COM2`,`COM3`]
     }
 
-    const GlobalEnviroment: GlobalEnviroment = ({...ResourcesEnviroment, global: true})
+    
+    type GlobalEnviroment = ResourcesEnviroment & { readonly isGlobalEnviroment: true}
+    const GlobalEnviroment: GlobalEnviroment = ({...ResourcesEnviroment, isGlobalEnviroment: true})
 
-    const aquire = (pid:number) => ZIO_.fromFunction( (g: ResourcesEnviroment) => { console.log();console.log(`openning port ${g.portIds[0].id}`); return g.resourceOpener(g.portIds[0].id) } )
-    const release = ZIO_.fromFunction( (r:Resource) => { console.log(`closing resource: #${r.kind.id}`); return undefined } )
-    const use = (r:Resource) => ZIO_.fromFunction( (g: ResourcesEnviroment) => {console.log(`using resource: ${r.kind.id}`)})
+     /** ZIO Abstraction */
+
+    const aquire = (port:string) => ZIO_.fromFunction( (enviroment: GlobalEnviroment) => enviroment.resourceOpener(port))
+    const release = (r: Resource) => r.close() 
+
+    const openPort = (portName:string) => ZManaged(aquire(portName), release)
 
 
-    const resource0 = ZManaged(aquire(0), release)
-    const resource1 = ZManaged(aquire(1), release)
-    const resource2 = ZManaged(aquire(2), release)
+    const ff = (portName: string) => 
+        openPort(portName).uses( port => {
+            console.log(`using resources! ${port.kind}`)
+            return ZIO_.succeed(`port sucessful used: ${port.kind   }`)
+        }).fmap( env => ZIO_.fromFuture(delay(2000) ) )
+    
+    const rr1 = ff('COM1')
+    const rr2 = ff('COM2')
 
-    const m  = [
-        resource0.uses( use ),
-        resource0.uses( use ),
-        resource1.uses( use ),
-        resource1.uses( use ),
-        resource2.uses( use ),
-        resource2.uses( use ),
-
-    ]
-
+    const rrr = 
+        rr1.fmap( s1 => 
+        rr2.map(  s2 => 
+            [s1, s2] as const    
+        ))
 
     console.log(`performing unsafe effects...`)
 
     // tslint:disable: no-expression-statement
-    const a = m.map( x => x.unsafeRun(ResourcesEnviroment))
-
+    const a = await rrr.unsafeRun(GlobalEnviroment) 
+    console.log(`Finished. output: ${a}`)
 
 
 
@@ -116,3 +115,5 @@ const Test = () => {
 
 // tslint:disable-next-line: no-expression-statement
 Test()
+
+
